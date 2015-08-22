fd60fb627ddfa661990c119a2161c0b0e57f036a
Gretchen Halasi-Kun <g.halasikun@gmail.com>
2015-08-22 21:04:55 +0000

    more tests

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "quote show page" do
+		quote = Quote.create(:author => 'Bugs Bunny', :saying => 'Whats up Doc?')
+		get :show, :id => quote.id
+		assert_response :success
+	end
+
+	test "quote show apge, not found" do
+		get :show, :id => 'OMG'
+		assert_response :not_found
+	end
 end
